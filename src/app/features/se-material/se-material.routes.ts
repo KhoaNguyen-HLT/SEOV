c0168104e182bff8aa8529f2a7731baae71583af
KhoaNguyen-HLT <khoanguyenhlt@gmail.com>
2026-05-24 22:52:09 +0700

    khoa:ok

---
+++ src/app/features/se-material/se-material.routes.ts
@@ -0,0 +1,13 @@
+import { Routes } from '@angular/router';
+
+
+export const materialRoutes: Routes = [
+
+  // {
+  //   path: 'se-andon-call',
+  //   loadComponent: () =>
+  //     import('./se-andon-call/se-andon-call')
+  //       .then(m => m.seAndonCallComponent),
+  // }
+
+];
---
+++ src/app/features/se-material/se-material.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../core/environments/environments';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class materialService {
+
+  private baseUrl = environment.apiUrl + "/material";
+
+  constructor(private http: HttpClient) { }
+
+  // ===== MATERIAL =====
+  // createMaterial(data: any): Observable<any> {
+  //   return this.http.post(`${this.baseUrl}/createAndon`, data);
+  // }
+
+  getLines(): Observable<any> {
+    return this.http.get(`${this.baseUrl}/getLines`);
+  }
+
+}
