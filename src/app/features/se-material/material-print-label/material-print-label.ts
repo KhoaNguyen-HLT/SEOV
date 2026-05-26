e25e55d150e1d276c0e45bcdeebdc38b4ef5f32b
KhoaNguyen-HLT <khoanguyenhlt@gmail.com>
2026-05-26 22:52:43 +0700

    khoa:print label

--- src/app/features/se-material/material-print-label/material-print-label.ts
+++ src/app/features/se-material/material-print-label/material-print-label.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {
-  FormsModule,
-  ReactiveFormsModule
+  FormBuilder,
+  FormGroup,
+  Validators
 } from '@angular/forms';
 
 import { NzDatePickerModule } from 'ng-zorro-antd/date-picker';
@@ -16,6 +18,8 @@ import { NzCardModule } from 'ng-zorro-antd/card';
 import { NzFormModule } from 'ng-zorro-antd/form';
 import { NzSelectModule } from 'ng-zorro-antd/select';
 import { NzInputNumberModule } from 'ng-zorro-antd/input-number';
+import { NgxPrintModule } from 'ngx-print';
+import { NzModalModule } from 'ng-zorro-antd/modal';
 
 @Component({
   selector: 'app-material-print-label',
@@ -34,200 +38,83 @@ import { NzInputNumberModule } from 'ng-zorro-antd/input-number';
     NzDividerModule,
     NzGridModule,
     NzIconModule,
-    NzSliderModule
+    NzSliderModule,
+    NgxPrintModule,
+    NzModalModule
   ],
   templateUrl: './material-print-label.html',
-  styleUrls: ['./material-print-label.css']
+  styleUrls: ['./material-print-label.css'],
 })
 export class MaterialPrintLabelComponent {
 
-  label = {
-    lotNo: 'AS23B378-20260526-001',
-    materialCode: 'AS23B378',
-    materialName: 'Thân ngoài ống bảo vệ',
-    qty: 200,
+  labelForm!: FormGroup;
+  constructor(
+  private fb: FormBuilder
+) {}
+
+ngOnInit(): void {
+  this.labelForm = this.fb.group({
+    materialCode: ['', Validators.required],
+    lotNo: ['', Validators.required],
+    qty: [0],
+    uom: ['PCS'],
+    location: [''],
+    labelCode: [''],
+    printQty: [1]
+  });
+}
+
+
+
+previewLabel(): void {
+
+  const lotNo =
+    this.labelForm.get('lotNo')?.value;
+
+  this.labelForm.patchValue({
+    labelCode: `LB_${lotNo}`
+  });
+
+}
+ 
+
+  labels = [
+  {
+    materialCode: 'NVL001',
+    lotNo: 'LOT001',
+    qty: 100,
     uom: 'PCS',
-    location: 'WH-A01',
-    expDate: '2026-11-26'
-  };
+    location: 'WH-A1'
+  },
+  {
+    materialCode: 'NVL002',
+    lotNo: 'LOT002',
+    qty: 50,
+    uom: 'PCS',
+    location: 'WH-A2'
+  }
+];
 
-  labelCount = 1;
+getQrUrl(lotNo: string): string {
+  return `http://localhost:8081/seov/qrcode/genQrcode/${lotNo}`;
+}
 
-  get qrUrl(): string {
-    return `http://localhost:8081/seov/qrcode/genQrcode/${this.label.lotNo}`;
+  // get qrUrl(): string {
+  //   return `http://localhost:8081/seov/qrcode/genQrcode/${this.label.lotNo}`;
+  // }
+
+
+
+  // Modal control
+  isVisible = false;
+
+  showModal(): void {
+    this.isVisible = true;
   }
 
-  printLabel(): void {
-
-    const count = Math.max(
-      1,
-      Number(this.labelCount || 1)
-    );
-
-    const labelsHtml = Array.from({ length: count })
-      .map(() => `
-        <div class="label">
-          <div class="title">MATERIAL LOT LABEL</div>
-
-          <div class="content">
-
-            <div class="info">
-              <div><b>LOT:</b> ${this.label.lotNo}</div>
-              <div><b>PART:</b> ${this.label.materialCode}</div>
-              <div><b>NAME:</b> ${this.label.materialName}</div>
-              <div><b>QTY:</b> ${this.label.qty} ${this.label.uom}</div>
-              <div><b>LOC:</b> ${this.label.location}</div>
-              <div><b>EXP:</b> ${this.label.expDate}</div>
-            </div>
-
-            <div class="qr">
-              <img
-                class="qr-img"
-                src="${this.qrUrl}" />
-            </div>
-
-          </div>
-        </div>
-      `)
-      .join('');
-
-
-
-    const printWindow = window.open(
-      '',
-      '_blank',
-      'width=100,height=100'
-    );
-
-    if (!printWindow) return;
-
-    printWindow.document.open();
-
-    printWindow.document.write(`
-      <html>
-        <head>
-          <title>Print Label</title>
-
-          <style>
-
-            @page {
-              size: 60mm 40mm;
-              margin: 0;
-            }
-
-            html,
-            body {
-              margin: 0;
-              padding: 0;
-              font-family: Arial, sans-serif;
-            }
-
-            .label {
-              width: 60mm;
-              height: 40mm;
-              border: 1px solid #000;
-              padding: 4mm;
-              box-sizing: border-box;
-              background: white;
-              overflow: hidden;
-
-              page-break-after: always;
-              break-after: page;
-            }
-
-            .label:last-child {
-              page-break-after: auto;
-              break-after: auto;
-            }
-
-            .title {
-              text-align: center;
-              font-weight: bold;
-              font-size: 14px;
-              border-bottom: 1px solid #000;
-              padding-bottom: 2mm;
-              margin-bottom: 2mm;
-            }
-
-            .content {
-              display: flex;
-              justify-content: space-between;
-              align-items: center;
-            }
-
-            .info {
-              width: 68%;
-              font-size: 11px;
-              line-height: 1.5;
-            }
-
-            .qr {
-              width: 16mm;
-              height: 16mm;
-            }
-
-            .qr img {
-              width: 100%;
-              height: 100%;
-              display: block;
-            }
-
-          </style>
-        </head>
-
-        <body>
-
-          ${labelsHtml}
-
-          <script>
-
-            const images =
-              Array.from(
-                document.querySelectorAll('.qr-img')
-              );
-
-            let loaded = 0;
-
-            function done() {
-              loaded++;
-
-              if (loaded >= images.length) {
-                setTimeout(function () {
-                  window.print();
-                  window.close();
-                }, 300);
-              }
-            }
-
-            if (images.length === 0) {
-              window.print();
-              window.close();
-            } else {
-
-              images.forEach(function(img) {
-
-                const image = img;
-
-                if (
-                  image.complete &&
-                  image.naturalWidth > 0
-                ) {
-                  done();
-                } else {
-                  image.onload = done;
-                  image.onerror = done;
-                }
-
-              });
-
-            }
-
-          </script>
-
-        </body>
-      </html>
-    `);
-
-    printWindow.document.close();
+  handleCancel(): void {
+    console.log('Button cancel clicked!');
+    this.isVisible = false;
   }
+
 }
